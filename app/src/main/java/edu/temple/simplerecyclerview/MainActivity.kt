f45f2049ddabec5019f7bc4515c0b23b428e5e0e
finnbassill <tun39698@temple.edu>
2023-09-26 12:13:30 -0400

    Changed layoutManager to grid & added more ints to array.

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,11 +14,11 @@ class MainActivity : AppCompatActivity() {
         // Step 1: Reference RecyclerView object
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        val arr = Array(10){(it + 1) * 5}
+        val arr = Array(100){(it + 1) * 5}
 
         //Step 2: Provide a LayoutManager
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = GridLayoutManager(this, 3)
 
         //Step 4: Provide a RecyclerView.Adapter
         recyclerView.adapter = NumberDisplayAdapter(arr)
